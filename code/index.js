af9567c8d0d1b489396ac403fdc1cbec95147604
minhduc1612112 <duc1612112@gmail.com>
2020-10-19 17:36:52 +0700

    first commit

---
+++ code/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+// Serve static files from /public
+app.use(express.static('public', {
+    setHeaders: (res, path, stat) => {
+        if (path.endsWith('.wasm')) {
+            res.set('Content-type', 'application/wasm')
+        }
+    }
+}))
+
+// Start server
+app.listen(3000, () => console.log('Server running on port 3000!'))
